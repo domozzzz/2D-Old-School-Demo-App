72902524206ba947d3a2962add302086f9cb5283
alist <alist@LAPTOP-4QNAS43J.lan>
2024-01-28 11:24:18 +1000

    plasma settings added

---
+++ src/main/Settings.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Settings {
+	
+	public static int SPEED = 1;
+	public static boolean RED = true;
+	public static boolean BLUE = true;
+	public static boolean GREEN = true;
+	public static int PATTERN = 1;
+
+}
